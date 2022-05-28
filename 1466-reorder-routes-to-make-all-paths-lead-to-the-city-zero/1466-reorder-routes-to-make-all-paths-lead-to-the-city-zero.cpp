50f5e8e8800b1e2663833708f035323555a42a51
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-05-28 10:41:31 +0530

    Time: 735 ms (25.87%), Space: 126.2 MB (31.71%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     
-    void dfs(int node, vector<vector<int>> &graph,int &res,vector<int>&vis, set<pair<int,int>> &st)
+    void dfs(int node, vector<vector<int>> &graph,int &res,int par, set<pair<int,int>> &st)
     {
-        vis[node]=1;
+        
         
         for(auto child :graph[node])
         {
-            if(vis[child]==0)
+            if(child!=par)
             {
                 if(st.find({node,child})!= st.end())res++;
-                dfs(child,graph,res,vis,st);
+                dfs(child,graph,res,node,st);
             }
         }
     }
     int minReorder(int n, vector<vector<int>>& connections) {
         
         vector<vector<int>> graph(n);
-        vector<int> vis(n,0);
+        //vector<int> vis(n,0);
         set<pair<int,int>> st;
         
         for(auto &x: connections)
@@ -27,7 +27,7 @@ public:
             st.insert({x[0],x[1]});
         }
         int res =0;
-        dfs(0,graph,res,vis,st);
+        dfs(0,graph,res,-1,st);
         return res;
         
     }
