9712be899ff42886be1f9c0a07089708047bb581
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-06-28 09:59:58 +0530

    Time: 71 ms (70.13%), Space: 17.3 MB (58.78%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+       
+        
+        vector<int> freq(26,0);
+        unordered_map<int,int> mp;
+        int del=0;
+        for(char c: s)
+        {
+          freq[c-'a']++;
+            
+        }
+        
+        for(int i=0;i<26;i++)
+            mp[freq[i]]++;
+        for(auto x: freq)
+        {
+            if(mp[x]>1)
+            {
+                int num =x;
+                while(num!=0 && mp[num]!=0)
+                {
+                    num--;
+                    del++;
+                }
+                if(num>0)
+                    mp[num]++;
+                mp[x]--;
+            }
+           
+        }
+        return del;
+        
+    }
+};
