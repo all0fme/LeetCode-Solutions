082a7e6960b100e4f0dd69d6afb093e13a989e83
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-07-13 08:53:44 +0530

    Time: 11 ms (73.59%), Space: 11.2 MB (59.85%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,38 +17,34 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(!head)
-            return NULL;
-        Node *tempHead = head;
-        Node *dummy= new Node(0);
-        Node* newHead = dummy;
-        unordered_map<Node*,Node*> mp;
+        Node* tempHead = head;
+        
         while(tempHead)
         {
-             if(mp.find(tempHead)!=mp.end())
-                {
-                    newHead->next = mp[tempHead];
-                }
-            else{
-                newHead->next = new Node(tempHead->val);
-            mp[tempHead] = newHead->next;
-            }
-            
-            if(tempHead->random)
-            {
-                if(mp.find(tempHead->random)!=mp.end())
-                {
-                    newHead->next->random = mp[tempHead->random];
-                }
-                else{
-                    newHead->next->random = new Node(tempHead->random->val);
-                    mp[tempHead->random] = newHead->next->random;
-                }
-            }
-            tempHead = tempHead->next;
-            newHead = newHead->next;
-                
+            Node * temp = new Node(tempHead->val);
+            temp->next = tempHead->next;
+            tempHead->next =temp;
+            tempHead = tempHead->next->next;
         }
-        return dummy->next;
+        tempHead = head;
+        while(tempHead)
+        {
+            if( tempHead->random)
+            {
+                 tempHead->next->random = tempHead->random->next;
+            }
+            tempHead = tempHead->next->next;
+        }
+        tempHead = head;
+        Node*  tail = new Node(-1);
+        Node* dummy = tail;
+        while(tempHead)
+        {
+            dummy->next = tempHead->next;
+            dummy = dummy->next;
+             tempHead->next = tempHead->next->next;
+            tempHead = tempHead->next;
+        }
+        return tail->next;
     }
 };
