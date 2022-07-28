c39eed17069b705499330fb7c3cab0ed252cf953
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-07-28 10:19:20 +0530

    Time: 26 ms (86.73%), Space: 6.9 MB (99.82%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -17,7 +17,7 @@ public:
             int l1 = expand(s,i,i);
             int l2= expand(s,i,i+1);
             int len = max(l1,l2);
-            if(len>(end-start)){
+            if(len>(end-start+1)){
                 start = i - (len-1)/2;
                 end = i+ len/2;
             }
