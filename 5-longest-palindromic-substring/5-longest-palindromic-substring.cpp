dd0b97a32fee1eeba0c4545a9c96b187ff0045d1
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-07-28 10:18:03 +0530

    Time: 43 ms (72.36%), Space: 7.1 MB (92.68%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
+    int expand(string &s, int L,int R){
+        while(L>=0 && R<s.size() && s[L]==s[R]){
+            L--;
+            R++;
+        }
+        return R-L-1;
+    }
     string longestPalindrome(string s) {
         
         int n = s.size();
+        int start =0,end=0;
         
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        int mxlen=1;
-        int st=0,e=0;
         for(int i=0;i<n;i++)
         {
-            dp[i][i]=1;
-            if(i!=n-1 && s[i]==s[i+1] )
-            {
-                
-                dp[i][i+1]= 1;
-                mxlen=2;
-                st=i;
-                e =i+1;
+            int l1 = expand(s,i,i);
+            int l2= expand(s,i,i+1);
+            int len = max(l1,l2);
+            if(len>(end-start)){
+                start = i - (len-1)/2;
+                end = i+ len/2;
             }
         }
-        
-        for(int i=n-3;i>=0;i--)
-        {
-            for(int j=i+2;j<n;j++){
-                if(s[i]==s[j] && dp[i+1][j-1]==1 ){
-                    dp[i][j]= 1;
-                   if((j-i+1)>mxlen)
-                   {
-                       mxlen = j-i+1;
-                       st = i;
-                       e =j;
-                   }
-                }
-                else{
-                    dp[i][j] =0;
-                }
-            }
-        }
-        
-        return s.substr(st,mxlen);
+        return s.substr(start,end-start+1);
     }
 };
