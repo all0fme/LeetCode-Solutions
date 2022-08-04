c6348e8ce432e98c708c6bd947e86733a10ae1cf
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-08-04 18:54:01 +0530

    Time: 77 ms (65.53%), Space: 31 MB (97.48%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,34 +1,14 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        
-        int sum =0;
-        int n = arr.size();
-        
-        for(int i=0;i<k;i++)
-        {
-            sum+=abs(arr[i]-x);
+   vector<int> findClosestElements(vector<int>& A, int k, int x) {
+        int left = 0, right = A.size() - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
         }
-        int ind = 0;
-        int tempsum=sum;
-        //cout<<n-k+1<<"\n";
-        for(int i=k;i<n;i++)
-        {
-           // cout<<tempsum<<"\n";
-            tempsum-=abs(arr[i-k]-x);
-            tempsum+=abs(arr[i]-x);
-            
-            if(tempsum<sum)
-            {
-                sum = tempsum;
-                ind = i-k+1;
-            }
-        }
-        vector<int> ans;
-        for(int i=ind;i<ind+k;i++)
-        {
-            ans.push_back(arr[i]);
-        }
-        return ans;
+        return vector<int>(A.begin() + left, A.begin() + left + k);
     }
 };
