9fe11748180c302a233039b0cb6a4fa23ce314f0
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-08-22 11:17:42 +0530

    Time: 4 ms (40.16%), Space: 5.9 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -4,19 +4,9 @@ public:
         
        
         bool b1 =((n&(n-1))==0);
-        if(!b1)
-            return false;
-        for(int i=0;i<31;i++)
-        {
-            if(n&(1<<i))
-            {
-                if(i%2==0)
-                    return true;
-                else
-                    return false;
-            }
-        }
-        return false;
+        bool b2 = (n-1)%3==0;
+        
+        return (b1&&b2);
         
     }
 };
