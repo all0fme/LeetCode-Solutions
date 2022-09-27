00656f58c64843c66d7df33dac26dbcca2fcdaf4
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-09-27 10:03:25 +0530

    Time: 105 ms (24.96%), Space: 21.5 MB (30.16%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-     string pushDominoes(string d) {
-        d = 'L' + d + 'R';
-        string res = "";
-        for (int i = 0, j = 1; j < d.length(); ++j) {
-            if (d[j] == '.') continue;
-            int middle = j - i - 1;
-            if (i > 0)
-                res += d[i];
-            if (d[i] == d[j])
-                res += string(middle, d[i]);
-            else if (d[i] == 'L' && d[j] == 'R')
-                res += string(middle, '.');
-            else
-                res += string(middle / 2, 'R') + string(middle % 2, '.') + string(middle / 2, 'L');
-            i = j;
+   string pushDominoes(string dominoes) {
+        int n = dominoes.size();
+        vector<int> left_r_dis(n + 1, 0);
+        vector<int> right_l_dis(n + 1, 0);
+        int r_pos = n + 1;
+        for (int i = 0; i < n; ++i) {
+            if (dominoes[i] == 'R') {
+                r_pos = i;
+            }
+            else if (dominoes[i] == 'L') {
+                r_pos = n + 1;
+            }
+            left_r_dis[i] = r_pos == n + 1 ? n + 1 : i - r_pos;
         }
-        return res;
-    }
+        int l_pos = -1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (dominoes[i] == 'L') {
+                l_pos = i;
+            }
+            else if (dominoes[i] == 'R') {
+                l_pos = -1;
+            }
+            right_l_dis[i] = l_pos == -1 ? n + 1 : l_pos - i;
+        }
+        
+        string result = dominoes;
+        for (int i = 0; i < n; ++i) {
+            if (left_r_dis[i] < right_l_dis[i]) result[i] = 'R';
+            else if (left_r_dis[i] > right_l_dis[i]) result[i] = 'L';
+            //else
+        }
+
+        return result;
+    } 
 };
