a49f09df1e182b8dd6ae6edf7f84b8b184b3cff3
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-10-11 10:20:07 +0530

    Time: 77 ms (90.14%), Space: 61.6 MB (24.04%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> pre(n);
-        vector<int> post(n);
-        
-        pre[0] = nums[0];
-        post[n-1] = nums[n-1];
-        for(int i=1;i<n;i++)
-        {
-            pre[i] = min(nums[i],pre[i-1]);
+   bool increasingTriplet(vector<int>& nums) {
+    int c1 = INT_MAX, c2 = INT_MAX;
+    for (int x : nums) {
+        if (x <= c1) {
+            c1 = x;           // c1 is min seen so far (it's a candidate for 1st element)
+        } else if (x <= c2) { // here when x > c1, i.e. x might be either c2 or c3
+            c2 = x;           // x is better than the current c2, store it
+        } else {              // here when we have/had c1 < c2 already and x > c2
+            return true;      // the increasing subsequence of 3 elements exists
         }
-        for(int i=n-2;i>=0;i--)
-        {
-            post[i] = max(nums[i],post[i+1]);
-        }
-        for(int i=1;i<n-1;i++)
-        {
-            if(pre[i-1]<nums[i] && post[i+1]>nums[i])
-                return true;
-        }
-        return false;
-        
-        
     }
+    return false;
+}
 };
