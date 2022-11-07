d478110882f9e95d56642e26b39b5ed8b1a6076f
Nikhil Ranjan Kumar <52556284+all0fme@users.noreply.github.com>
2022-11-07 10:02:01 +0530

    Time: 5 ms (15.47%), Space: 5.9 MB (82.75%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        
+        string s = to_string(num);
+       int i=0;
+        while(i<s.size() && s[i]!='6')
+            i++;
+        
+        if(i<s.size())
+        s[i] ='9';
+       
+      return stoi(s);
+        
+    }
+};
